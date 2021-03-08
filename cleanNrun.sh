1cf706859ca554f40510920ec7f156fa4004e31d
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2021-03-08 14:15:32 -0600

    Fixing orchestrator to work locally

--- cleanNrun.sh
+++ cleanNrun.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 #sample runs:
-#sh cleanNrun.sh X2_2D_3bin process.dat WD/X2_2D_3bin
-#sh cleanNrun.sh SumOfDiffPowers_2D_3bin process.dat WD/SumOfDiffPowers_2D_3bin
+#sh cleanNrun.sh Parameters/X2_2D_3bin process.dat WD/X2_2D_3bin
+#sh cleanNrun.sh Parameters/SumOfDiffPowers_2D_3bin process.dat WD/SumOfDiffPowers_2D_3bin
 mkdir -p $3
 mkdir -p $3/conf
 mkdir -p $3/logs
@@ -11,9 +11,9 @@ cp decaf-henson_python $3/.
 cp $2 $3/. #process.dat
 cp *.py $3/. #maybe give full path in JSON 
  
-cp Parameters/$1/algoparams_bk.json $3/conf/algoparams_bk.json
-cp Parameters/$1/data.json $3/conf/data.json
-cp Parameters/$1/weights $3/conf/weights
+cp $1/algoparams_bk.json $3/conf/algoparams_bk.json
+cp $1/data.json $3/conf/data.json
+cp $1/weights $3/conf/weights
 #rm -r logs/*
 cd $3
 mpirun -np 1 ./decaf-henson_python
--- orchestrator.py
+++ orchestrator.py
@@ -1,9 +1,6 @@
-import pyhenson as h
 import sys
 import os
 import argparse
-import json
-import numpy as np
 
 class SaneFormatter(argparse.RawTextHelpFormatter,
 			argparse.ArgumentDefaultsHelpFormatter):
@@ -24,43 +21,22 @@ if __name__ == "__main__":
 	memorymap = apprentice.tools.putInMemoryMap(memoryMap=None, key="file", value=args.ALGOPARAMS)
 	apprentice.tools.putInMemoryMap(memoryMap=memorymap, key="debug", value=args.DEBUG)
 
-
-	# # Not used for now
-	# with open (args.ALGOPARAMS, 'r') as f:
-	# 	algoparamsds = json.load(f)
-	# tr_radius = algoparamsds['tr']['radius']
-	# # tc = TestClass()
-	# # c= np.array("ss1","ss"])
-	# # print(c)
-	# # h.add("classss",c)
-	# h.add("tr_radius",tr_radius)
-	# #orc@04-03: adding array comm via henson
-	# tr_center = np.array(algoparamsds['tr']['center'])
-	# param_bounds = np.array(algoparamsds['param_bounds'])
-	# # h.add("param_bounds",param_bounds)
-	# h.add("tr_center", tr_center)
-
-	# num_iter = args.ITERNO
-	# debug = args.DEBUG
-	# h.add("debug", debug)
 	for k in range(apprentice.tools.getFromMemoryMap(memoryMap=memorymap,key="max_iteration")):
 		apprentice.tools.putInMemoryMap(memoryMap=memorymap, key="iterationNo", value=k)
 		pyhenson = False
 		try:
+			import pyhenson as h
 			h.add("MemoryMap", memorymap)
 			pyhenson = True
 		except:
-			print(
-				"Standalone` run detected. I will store data structures in files "
-				"for communication between tasks")
-			ds = {"MemoryMap":memorymap.tolist()}
-			with open("/tmp/memorymap.json",'w') as f:
-				json.dump(ds,f,indent=4)
+			apprentice.tools.writeMemoryMap(memoryMap=memorymap)
 
 		if apprentice.tools.getFromMemoryMap(memoryMap=memorymap,key="debug"):
 			print("orchestrator: yielding to other tasks, at iter", k)
 		sys.stdout.flush()
-		if pyhenson: h.yield_()
+		if pyhenson:
+			import pyhenson as h
+			h.yield_()
 
 	print("===terminating the workflow after", k+1, "iterations@ORCHESTRATOR===")
 	sys.stdout.flush()
