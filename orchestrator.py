c086dd5596d2ebf1e25f35552423b0e3e5d290e6
Mohan Krishnamoorthy <mkrishnamoorthy@beboplogin3.lcrc.anl.gov>
2021-03-08 13:32:07 -0600

    memory map

--- orchestrator.py
+++ orchestrator.py
@@ -12,44 +12,55 @@ class SaneFormatter(argparse.RawTextHelpFormatter,
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Orchestrator for the workflow',
 			formatter_class=SaneFormatter)
-	parser.add_argument("--iterno", dest="ITERNO", type=int, default=2,
-			help="Max iteration number")
+	# parser.add_argument("--iterno", dest="ITERNO", type=int, default=2,
+	# 		help="Max iteration number")
 	parser.add_argument("-v", "--debug", dest="DEBUG", type=int, default=0,
 			help="Turn on some debug messages")
 	parser.add_argument("-a", dest="ALGOPARAMS", type=str, default=None,
 			help="Algorithm Parameters (JSON)")
 
-	pyhenson = True
-	try:
-		h.add("TestPyHenson",np.Infinity)
-	except:
-		print("Standalone` run detected. I will store data structures in files fon communication between tasks")
-		pyhenson = False
-
 	args = parser.parse_args()
-	# Not used for now
-	with open (args.ALGOPARAMS, 'r') as f:
-		algoparamsds = json.load(f)
-	tr_radius = algoparamsds['tr']['radius']
-	# tc = TestClass()
-	# c= np.array("ss1","ss"])
-	# print(c)
-	# h.add("classss",c)
-	h.add("tr_radius",tr_radius)
-	#orc@04-03: adding array comm via henson
-	tr_center = np.array(algoparamsds['tr']['center'])
-	param_bounds = np.array(algoparamsds['param_bounds'])
-	# h.add("param_bounds",param_bounds)
-	h.add("tr_center", tr_center)
-	num_iter = args.ITERNO
-	debug = args.DEBUG
-	h.add("debug", debug)
-	for k in range(num_iter):
-		h.add("iter", k)
-		if debug==1: print("orchestrator: yielding to other tasks, at iter", k)
-		sys.stdout.flush()
-		h.yield_()
+	import apprentice
+	memorymap = apprentice.tools.putInMemoryMap(memoryMap=None, key="file", value=args.ALGOPARAMS)
+	apprentice.tools.putInMemoryMap(memoryMap=memorymap, key="debug", value=args.DEBUG)
 
+
+	# # Not used for now
+	# with open (args.ALGOPARAMS, 'r') as f:
+	# 	algoparamsds = json.load(f)
+	# tr_radius = algoparamsds['tr']['radius']
+	# # tc = TestClass()
+	# # c= np.array("ss1","ss"])
+	# # print(c)
+	# # h.add("classss",c)
+	# h.add("tr_radius",tr_radius)
+	# #orc@04-03: adding array comm via henson
+	# tr_center = np.array(algoparamsds['tr']['center'])
+	# param_bounds = np.array(algoparamsds['param_bounds'])
+	# # h.add("param_bounds",param_bounds)
+	# h.add("tr_center", tr_center)
+
+	# num_iter = args.ITERNO
+	# debug = args.DEBUG
+	# h.add("debug", debug)
+	for k in range(apprentice.tools.getFromMemoryMap(memoryMap=memorymap,key="max_iteration")):
+		apprentice.tools.putInMemoryMap(memoryMap=memorymap, key="iterationNo", value=k)
+		pyhenson = False
+		try:
+			h.add("MemoryMap", memorymap)
+			pyhenson = True
+		except:
+			print(
+				"Standalone` run detected. I will store data structures in files "
+				"for communication between tasks")
+			ds = {"MemoryMap":memorymap.tolist()}
+			with open("/tmp/memorymap.json",'w') as f:
+				json.dump(ds,f,indent=4)
+
+		if apprentice.tools.getFromMemoryMap(memoryMap=memorymap,key="debug"):
+			print("orchestrator: yielding to other tasks, at iter", k)
+		sys.stdout.flush()
+		if pyhenson: h.yield_()
 
 	print("===terminating the workflow after", k+1, "iterations@ORCHESTRATOR===")
 	sys.stdout.flush()
