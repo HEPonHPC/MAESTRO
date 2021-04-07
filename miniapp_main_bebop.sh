6103c30a88c947a82f6ca14445217ecfc8804f15
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2021-04-07 12:12:51 -0500

    beb

---
+++ miniapp_main_bebop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -r WD;
+sh cleanNrun_miniapp.sh Parameters/Miniapp process.dat WD/Miniapp;
+cd WD/Miniapp ||exit ;
+python orchestrator.py   -a conf/algoparams_bk.json;
+status=0;
+until [ $status -ne 0 ]
+do
+  python mc_miniapp.py -e conf/data.json -c process.dat -o initial -b
+  python buildInterpol.py -c process.dat
+  mpirun -np 4 python mc_miniapp.py -e conf/data.json -c process.dat -o multi -b
+  python approx.py -e conf/data.json -w conf/weights
+  python chi2.py -e conf/data.json -w conf/weights -c process.dat
+  python mc_miniapp.py -e conf/data.json -c process.dat -o single -b
+  python newBox.py -w conf/weights -e conf/data.json
+  status=$?
+  python orchestrator.py  -c -a conf/algoparams_bk.json
+done
+cd ../..
+
