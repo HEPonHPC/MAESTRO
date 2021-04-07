ad01a3d49b7ef630b75f2887c47e51da4a855534
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2021-04-07 11:05:34 -0500

    Decaf Henson miniapp

---
+++ cleanNrun_miniapp.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#sample runs:
+#sh cleanNrun.sh Parameters/X2_2D_3bin process.dat WD/X2_2D_3bin
+#sh cleanNrun.sh Parameters/SumOfDiffPowers_2D_3bin process.dat WD/SumOfDiffPowers_2D_3bin
+mkdir -p $3
+mkdir -p $3/conf
+mkdir -p $3/logs
+cp decaf-henson_miniapp.json $3/decaf-henson.json
+cp decaf-henson_python $3/.
+cp $2 $3/. #process.dat
+cp *.py $3/. #maybe give full path in JSON
+
+cp $1/algoparams_bk.json $3/conf/algoparams_bk.json
+cp $1/data.json $3/conf/data.json
+cp $1/weights $3/conf/weights
+#rm -r logs/*
+cd $3
+mpirun -np 5 ./decaf-henson_python
