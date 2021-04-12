9c6c25bed8eb694773490ca90dc356a8116cb18a
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2021-04-12 15:56:32 -0500

    Miniapp main changing options

--- miniapp_main.sh
+++ miniapp_main.sh
@@ -35,7 +35,7 @@ cp $miniappParam/weights $WDdir/conf/weights
 #sh cleanNrun_miniapp.sh Parameters/Miniapp process.dat WD/Miniapp;
 currDir=`pwd`
 cd $WDdir ||exit ;
-python orchestrator.py -o 11  -a conf/algoparams_bk.json;
+python orchestrator.py -o 10  -a conf/algoparams_bk.json;
 status=0;
 until [ $status -ne 0 ]
 do
@@ -47,7 +47,7 @@ do
   python mc_miniapp.py -e conf/data.json -c process.dat -o single $bebop
   python newBox.py -w conf/weights -e conf/data.json
   status=$?
-  python orchestrator.py -o 11 -c -a conf/algoparams_bk.json
+  python orchestrator.py -o 10 -c -a conf/algoparams_bk.json
 done
 cd $currDir ||exit ;
 
