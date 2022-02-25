040f9718a205eb4c6fdff8531f0999ff63ecda44
Mohan Krishnamoorthy <mohansk2000@gmail.com>
2022-02-25 08:39:43 -0500

    renamed:    mfstrodf/subproblem.py -> mfstrodf/fstructure.py

--- mfstrodf/__init__.py
+++ mfstrodf/__init__.py
@@ -3,6 +3,6 @@ from mfstrodf.settings import Settings, WorkingDirectory, AlgorithmStatus
 from mfstrodf.util import OutputLevel,DiskUtil,ParameterPointUtil
 from mfstrodf.sample import InterpolationSample
 from mfstrodf.model import ModelConstruction
-from mfstrodf.subproblem import Fstructure
+from mfstrodf.fstructure import Fstructure
 from mfstrodf.tr import TrAmmendment
 from mfstrodf.mpi4py_ import MPI_, COMM_
