946d2c8afae660cea5af207a53ec809b1691d456
Mohan Krishnamoorthy <mohansk2000@gmail.com>
2022-03-28 15:32:48 -0400

    Workflow files for simpleapp and deterministicapp

---
+++ workflow/deterministicapp/run_workflow.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+#sample runs:
+# sh run_workflow.sh /tmp/deterministicapp/WD_workflow ../../../parameter_config_backup/deterministicapp/
+# sh run_workflow.sh ../../../../log/workflow/deterministicapp/WD_workflow ../../../parameter_config_backup/deterministicapp/
+rm -rf $1
+mkdir -p $1
+mkdir -p $1/conf
+mkdir -p $1/logs
+cp decaf-henson.json $1/decaf-henson.json
+cp ../bin/decaf-henson_python $1/.
+#cp $2 $3/. #process.dat
+cp ../../mfstrodf/optimizationtask.py $1/.
+cp ../../mfstrodf/mc/bin/deterministicapp.py $1/.
+
+cp $2/algoparams.json $1/conf/algoparams.json
+cp $2/config_workflow.json $1/conf/config.json
+
+#rm -r logs/*
+cd $1 || exit
+mpirun -np 4 ./decaf-henson_python
---
+++ workflow/simpleapp/run_workflow.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+#sample runs:
+# sh run_workflow.sh /tmp/simpleapp/WD_workflow ../../../parameter_config_backup/simpleapp/
+# sh run_workflow.sh ../../../../log/workflow/simpleapp/WD_workflow ../../../parameter_config_backup/simpleapp/
+rm -rf $1
+mkdir -p $1
+mkdir -p $1/conf
+mkdir -p $1/logs
+cp decaf-henson.json $1/decaf-henson.json
+cp ../bin/decaf-henson_python $1/.
+#cp $2 $3/. #process.dat
+cp ../../mfstrodf/optimizationtask.py $1/.
+cp ../../mfstrodf/mc/bin/simpleapp.py $1/.
+
+cp $2/algoparams.json $1/conf/algoparams.json
+cp $2/config_workflow.json $1/conf/config.json
+
+#rm -r logs/*
+cd $1 || exit
+mpirun -np 4 ./decaf-henson_python
