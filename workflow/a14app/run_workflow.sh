7f09775a7b89c883cda215c54068844bacedc503
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2022-05-06 12:39:06 -0400

    Workflow decaf henson and run shell script for a14

---
+++ workflow/a14app/run_workflow.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+#sample runs:
+# sh run_workflow.sh /tmp/a14app/WD_workflow ../../parameter_config_backup/a14app
+# sh run_workflow.sh ../../../log/workflow/a14app/WD_workflow ../../parameter_config_backup/a14app
+rm -rf $1
+mkdir -p $1
+mkdir -p $1/conf
+mkdir -p $1/logs
+cp decaf-henson.json $1/decaf-henson.json
+cp ../bin/decaf-henson_python $1/.
+cp $2/*.cmnd $1/conf/.
+cp ../../mfstrodf/optimizationtask.py $1/.
+#cp ../../mfstrodf/mc/bin/miniapp.py $1/.
+
+cp $2/algoparams.json $1/conf/algoparams.json
+cp $2/config_workflow.json $1/conf/config.json
+
+#rm -r logs/*
+cd $1 || exit
+mpirun -np 32 ./decaf-henson_python
