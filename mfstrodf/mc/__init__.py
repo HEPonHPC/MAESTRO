c3b6c8b45c15be1dd06d1e5f6c3d9cba2d310441
Mohan Krishnamoorthy <mkrishnamoorthy@anl.gov>
2022-05-17 08:26:12 -0400

    Setup bug fix

--- mfstrodf/mc/__init__.py
+++ mfstrodf/mc/__init__.py
@@ -1,5 +1,4 @@
 from mfstrodf.mc.mctask import *
 from mfstrodf.mc.simpleapp import *
 from mfstrodf.mc.miniapp import *
-# from mfstrodf.mc.miniapp import *
-# from mfstrodf.mc.a14app import *
+from mfstrodf.mc.a14app import *
